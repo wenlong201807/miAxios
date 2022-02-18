8eaa16cb1d3ea43ff3ce1350270932c4c145d74b
zhuwenlong <zhuwenlong@autohome.com.cn>
2022-02-18 16:44:45 +0800

    feat:拦截器的添加，成功

--- README.md
+++ README.md
@@ -10,4 +10,5 @@
 ## 参考资料
 - [axios中文文档|axios中文网](http://www.axios-js.com/zh-cn/docs/)
 - [rollup启动本地服务](https://copyfuture.com/blogs-details/20200612180652285uwvetng6iys65up)
+- [rollup启动本地服务server -> npm](https://www.npmjs.com/package/rollup-plugin-serve)
 - [XMLHttpRequest—必知必会](https://www.jianshu.com/p/918c63045bc3/)
--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default {
   input: 'lib/axios.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'iife',
+    format: 'iife', // 生成立即执行函数
     name: 'axios',
   },
   plugins: [
@@ -14,10 +14,23 @@ export default {
     commonjs(),
     // 本地服务器
     serve({
-      open: true, // 自动打开页面
+      open: true, // 自动打开页面 // 无效
+      host: 'localhost',
       port: 8008,
-      openPage: './index.html', // 打开的页面
-      contentBase: '',
+      openPage: './index.html', // 打开的页面 // 未测试
+      verbose: true, // 无效
+      contentBase: '', // 未测试
+      headers: { // 未测试
+        'Access-Control-Allow-Origin': '*',
+        foo: 'bar'
+      },
+    //   onListening: function (server) { // 失败，用法错误
+    //     const address = server.getAddress()
+    //     const host = address.host === '::' ? 'localhost' : address.host
+    //     // by using a bound function, we can access options as `this`
+    //     const protocol = this.https ? 'https' : 'http'
+    //     console.log(`Server listening at ${protocol}://${host}:${address.port}/`)
+    //   }
     }),
   ],
 };
